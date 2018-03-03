8e07775725592249dfe688062cebe8ea3bf838b2
Joshua C. Burt <joshburt@shapeandshare.com>
2018-03-03 15:59:48 -0700

    users merchants

---
+++ dbo/stored_procs/getUserMerchantTransformsByGUID.sql
@@ -0,0 +1,16 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getUserMerchantTransformsByGUID`(
+	IN target_guid VARCHAR(255)
+)
+BEGIN
+SELECT DISTINCT st1.store_name
+	FROM user u1
+JOIN store s1
+	ON u1.user_id = s1.user_id
+JOIN merchant_transforms mt1
+	ON s1.store_id = mt1.to_store_id
+JOIN store_type st1
+	ON st1.store_id = s1.store_id
+WHERE u1.guid = target_guid
+	AND u1.active > 0
+ORDER BY st1.store_name;
+END
