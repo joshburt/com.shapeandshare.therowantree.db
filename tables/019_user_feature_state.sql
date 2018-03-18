665d1cbd7417cb4a70d47320d52e829fe67feb6f
Joshua C. Burt <joshburt@shapeandshare.com>
2018-03-18 13:53:53 -0600

    default to the default location and state!

--- tables/019_user_feature_state.sql
+++ tables/019_user_feature_state.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `user_feature_state` (
   `user_id` int(11) NOT NULL,
-  `feature_id` int(11) NOT NULL,
-  `feature_index` int(11) NOT NULL,
+  `feature_id` int(11) NOT NULL DEFAULT '1',
+  `feature_index` int(11) NOT NULL DEFAULT '1',
   UNIQUE KEY `unique_user_id_feature_id_combos` (`user_id`,`feature_id`),
   KEY `fk_feature_id_user_feature_state_feature_type_idx` (`feature_id`,`feature_index`),
   KEY `fk_user_id_user_feature_state_user_id_idx` (`user_id`),
