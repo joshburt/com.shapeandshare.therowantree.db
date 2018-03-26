55684ef1169e6979eac12ab0d2d89995a1657df0
Joshua C. Burt <joshburt@shapeandshare.com>
2018-03-25 17:35:51 -0700

    UGH, apparently this doesn't actual work unless its EXPLICITLY wrapped in a transaction.  Time to go back over them all..

--- stored_procs/sendUserNotification.sql
+++ stored_procs/sendUserNotification.sql
@@ -1,8 +1,10 @@
-CREATE PROCEDURE `sendUserNotification` (
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sendUserNotification`(
 	IN target_user_id INT(11),
     IN target_notification VARCHAR(2048)
 )
 BEGIN
-	INSERT INTO user_notification (user_id, message) VALUES
-		(target_user_id, target_notification);
+	START TRANSACTION;
+		INSERT INTO user_notification (user_id, message) VALUES
+			(target_user_id, target_notification);
+	COMMIT;
 END
