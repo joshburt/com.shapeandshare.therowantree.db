09969598980e5e9ad2144b947b6b5c7a5b12adfa
Joshua C. Burt <joshburt@shapeandshare.com>
2018-03-25 17:34:15 -0700

    used by sever

---
+++ stored_procs/deltaUserPopulationByID.sql
@@ -0,0 +1,31 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deltaUserPopulationByID`(
+	IN target_id INT(11),
+    IN amount INT(11)
+)
+BEGIN
+	DECLARE new_population INT(11) DEFAULT 0;
+    DECLARE current_population INT(11);
+
+	START TRANSACTION;
+	
+	SET current_population = (SELECT population FROM user WHERE user_id = target_id);
+	IF (current_population IS NULL)
+	THEN
+		SET new_population = 0;
+	ELSE
+		SELECT population
+			INTO current_population
+		FROM user
+			WHERE user_id = target_id;
+		SET new_population = current_population + amount;
+	END IF;
+	
+	IF (new_population >= 0)
+	THEN
+		UPDATE user
+			SET population = new_population
+		WHERE user_id = target_id;
+	END IF;
+    
+	COMMIT;
+END
