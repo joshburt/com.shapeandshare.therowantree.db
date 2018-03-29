6f5518e594e1459e883b86f35ef8875ccfd1f505
Joshua C. Burt <joshburt@shapeandshare.com>
2018-03-28 19:58:31 -0700

    default user stores by raw store name

---
+++ stored_procs/deltaUserStoreByStoreName.sql
@@ -0,0 +1,28 @@
+DROP procedure IF EXISTS `deltaUserStoreByStoreName`;
+
+DELIMITER $$
+CREATE PROCEDURE `deltaUserStoreByStoreName` (
+	IN `my_user_id` INT(11),
+	IN `my_store_name` VARCHAR(255),
+	IN `my_amount` FLOAT
+)
+BEGIN
+	START TRANSACTION;
+    
+    SET @amount = (SELECT amount FROM store WHERE user_id=my_user_id AND store_id=my_store_id);
+    SET @my_store_id = (SELECT store_id FROM store_type WHERE store_name = my_store_name);
+    IF (@amount IS NULL)
+    THEN
+		INSERT INTO store (user_id, store_id, amount) VALUES (my_user_id, @my_store_id, my_amount);
+	ELSE
+		UPDATE store
+			SET amount = (@amount  + my_amount)
+		WHERE user_id=my_user_id 
+			AND store_id=@my_store_id;
+    END IF;
+    
+    COMMIT;
+END$$
+
+DELIMITER ;
+
