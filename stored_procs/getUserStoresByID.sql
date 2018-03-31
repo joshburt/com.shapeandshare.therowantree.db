5e630032cdf90251a1d54985d13f494d816fcdb9
Joshua C. Burt <joshburt@shapeandshare.com>
2018-03-30 19:44:43 -0700

    get user stores by id

---
+++ stored_procs/getUserStoresByID.sql
@@ -0,0 +1,18 @@
+DROP procedure IF EXISTS `getUserStoresByID`;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getUserStoresByID`(
+	IN target_user_id INT(11)
+)
+BEGIN
+	SELECT st1.store_name, st1.store_description, s1.amount
+		FROM store s1
+		JOIN user u1
+			ON u1.user_id = s1.user_id
+		JOIN store_type st1
+			ON st1.store_id = s1.store_id
+	WHERE u1.user_id = target_user_id;
+END$$
+
+DELIMITER ;
+
