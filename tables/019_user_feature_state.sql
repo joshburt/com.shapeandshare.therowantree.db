019fb66b878b6f5d78d6c0f588c02f30527a2a08
Joshua C. Burt <joshburt@shapeandshare.com>
2018-12-26 12:13:33 -0700

    adding primary key for ef core suppport

--- tables/019_user_feature_state.sql
+++ tables/019_user_feature_state.sql
@@ -1,7 +1,10 @@
 CREATE TABLE IF NOT EXISTS `user_feature_state` (
+  `user_feature_state_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `feature_id` int(11) NOT NULL DEFAULT '1',
   `feature_index` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`user_feature_state_id`),
+  UNIQUE KEY `user_feature_state_id_UNIQUE` (`user_feature_state_id`),
   UNIQUE KEY `unique_user_id_feature_id_combos` (`user_id`,`feature_id`),
   KEY `fk_feature_id_user_feature_state_feature_type_idx` (`feature_id`,`feature_index`),
   KEY `fk_user_id_user_feature_state_user_id_idx` (`user_id`),
