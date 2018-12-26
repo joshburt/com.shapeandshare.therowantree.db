9a57fb5e8260259db39a07ca1e1e8e2ad510d20a
Joshua C. Burt <joshburt@shapeandshare.com>
2018-12-26 11:46:21 -0700

    added primary key for ef core support

--- tables/012_merchant_transforms.sql
+++ tables/012_merchant_transforms.sql
@@ -1,7 +1,10 @@
 CREATE TABLE IF NOT EXISTS `merchant_transforms` (
+  `merchant_transforms_id` int(11) NOT NULL AUTO_INCREMENT,
   `to_store_id` int(11) NOT NULL,
   `from_store_id` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
+  PRIMARY KEY (`merchant_transforms_id`),
+  UNIQUE KEY `merchant_transforms_id_UNIQUE` (`merchant_transforms_id`),
   UNIQUE KEY `fk_unique_combos` (`to_store_id`,`from_store_id`),
   KEY `fk_to_store_id_merchant_transforms_store_type_idx` (`to_store_id`),
   KEY `fk_from_store_id_merchant_transforms_store_type` (`from_store_id`),
