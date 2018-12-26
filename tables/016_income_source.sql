e88e73ab19f61e8bafd11cdebed3663254f2c1dd
Joshua C. Burt <joshburt@shapeandshare.com>
2018-12-26 11:32:12 -0700

    added primary key for efcore support

--- tables/016_income_source.sql
+++ tables/016_income_source.sql
@@ -1,7 +1,10 @@
 CREATE TABLE IF NOT EXISTS `income_source` (
+  `income_source_store_id` int(11) NOT NULL AUTO_INCREMENT,
   `income_source_id` int(11) NOT NULL,
   `store_id` int(11) NOT NULL,
   `amount` float NOT NULL,
+  PRIMARY KEY (`income_source_store_id`),
+  UNIQUE KEY `income_source_store_id_UNIQUE` (`income_source_store_id`),
   UNIQUE KEY `indx_unique_combos` (`income_source_id`,`store_id`),
   KEY `fk_income_source_id_income_source_type_income_idx` (`income_source_id`),
   KEY `fk_store_id_store_type_income_idx` (`store_id`),
