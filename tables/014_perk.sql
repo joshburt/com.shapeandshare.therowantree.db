6a0a3c3518d7928922a60dc0bfc7228dc86f9e39
Joshua C. Burt <joshburt@shapeandshare.com>
2018-12-26 11:52:03 -0700

    add primary key for ef core support

--- tables/014_perk.sql
+++ tables/014_perk.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS `perk` (
+  `user_perk_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `perk_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_perk_id`),
+  UNIQUE KEY `user_perk_id_UNIQUE` (`user_perk_id`),
   UNIQUE KEY `idx_user_id_perk_id` (`user_id`,`perk_id`),
   KEY `fk_user_id_perk_user_idx` (`user_id`),
   KEY `fk_perk_id_perk_perk_type_idx` (`perk_id`),
