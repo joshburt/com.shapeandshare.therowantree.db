7ef223aa944d6ac90fdc69195fd2e1cda7e0bd84
Joshua Burt <joshburt@shapeandshare.com>
2022-09-04 16:21:15 -0700

    updated notify

---
+++ stored_procs/sendUserNotificationByGUID.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+DROP procedure IF EXISTS `sendUserNotification`;
+
+CREATE PROCEDURE `sendUserNotification`(
+	IN target_user_guid INT(11),
+    IN target_notification VARCHAR(2048)
+)
+BEGIN
+	START TRANSACTION;
+        SET target_user_id = (SELECT user_id FROM user WHERE guid = target_user_guid);
+		INSERT INTO user_notification (user_id, message) VALUES
+			(target_user_id, target_notification);
+	COMMIT;
+END$$
+
+DELIMITER ;
